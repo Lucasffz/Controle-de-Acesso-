a2625b0cab6de70d555d41e6d8e0e6fd16285ca3
isaaccentenario <isaac.centenario@gmail.com>
2017-12-04 10:29:45 -0300

    Adição do objeto do laboratório no acesso

--- src/Trabalhofinal/poo/controler/Controle.java
+++ src/Trabalhofinal/poo/controler/Controle.java
@@ -97,10 +97,10 @@ public class Controle {
 		return laboratorios;
 	}
 	
-	public void adcionarAcesso(String id,String senha,String status) {
+	public void adcionarAcesso(String id,String senha,String status, Laboratorio laboratorio) {
 		String dataAtual = formatarData.format(data);
 		String hora = formatarHora.format(data);
-		Acesso acesso = new Acesso(id,senha,status,dataAtual,hora);
+		Acesso acesso = new Acesso(id,senha,status,dataAtual,hora, laboratorio);
 		acessos.add(acesso);
 	}
 	
